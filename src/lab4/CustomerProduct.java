6506e1329e916a1e1f5f83006510d528eb07d6d6
sketlafox <es.ahmed.bakr2023@alexu.edu.eg>
2025-10-17 12:54:09 +0300

    code writing

--- src/lab4/CustomerProduct.java
+++ src/lab4/CustomerProduct.java
@@ -1,4 +1,47 @@
 package lab4;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class CustomerProduct {
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+
+    public CustomerProduct(String customerSSN, String productID, LocalDate
+            purchaseDate){
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+    }
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+    public String getProductID(){
+        return productID;
+    }
+    public LocalDate getPurchaseDate(){
+        return purchaseDate;
+    }
+    public boolean isPaid(){
+        return paid;
+    }
+    public void setPaid(boolean paid){
+        this.paid = paid;
+    }
+    public String lineRepresentation(){
+        StringBuilder Date = new StringBuilder(purchaseDate.toString());
+        Date.reverse();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return customerSSN+","+productID+","+Date+","+paid;
+    }
+
+    public String getSearchKey(){
+        return customerSSN;
+    }
+
+
 }
+
--- src/lab4/Product.java
+++ src/lab4/Product.java
@@ -1,4 +1,31 @@
 package lab4;
 
 public class Product {
+    private String productName;
+    private String productID;
+    private String manufacturerName;
+    private String supplierName;
+    private float price;
+    private int quantity;
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public String lineRepresentation(){
+        return productID+","+productName+","+manufacturerName+","+supplierName+","+quantity+","+price;
+    }
+    public String getSearchKey(){
+        return productID;
+    }
 }

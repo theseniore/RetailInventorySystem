134b076eff60ac78df44d4533211b68eba9b0617
theseniore <hossamdessoky.1970@gmail.com>
2025-10-17 22:48:33 +0300

    menu updated

--- src/lab4/Menu.java
+++ src/lab4/Menu.java
@@ -1,22 +1,12 @@
 package lab4;
 
-import java.util.Scanner;
-
 public class Menu {
     public static void mainMenu()
     {
         System.out.println("Welcome to retail managment system!");
         System.out.println("For Admins enter 1\nFor employees enter 2\nTo exit enter 3");
-        Scanner in = new Scanner(System.in);
-        int n=0;
-        while (true){
-            System.out.println("enter an integer");
-        if (in.hasNextInt()){
-n=in.nextInt();
-break;
-        }
-        }
-        switch(n)
+        // switch case for admin and employees
+        switch()
         {
             case 1:
                 adminMenu();

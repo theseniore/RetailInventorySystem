938c35d235d4fa98a65f440f63efa8bdead685ea
AHMEDIKO <ahmeddmohammed7@gmail.com>
2025-10-18 15:03:36 +0300

    added validation class

---
+++ src/lab4/Validations.java
@@ -0,0 +1,23 @@
+package lab4;
+
+public class Validations {
+
+    public static boolean emailValid(String email){
+        return email != null && email.matches("^[\\w.-]+@[\\w.-]+\\.\\w+$");
+    }
+    public static boolean phoneValid(String phone){
+        return phone != null && phone.matches("\\d{11}");
+    }
+    public static boolean ssnValid(String ssn){
+        return ssn != null && ssn.matches("\\d{14}");
+    }
+    public static boolean nameValid(String name){
+        return name != null && name.matches("[a-zA-Z ]+");
+    }
+    public static boolean quantityValid(int quantity){
+        return quantity>=0;
+    }
+    public static boolean priceValid(float price){
+        return price>=0;
+    }
+}

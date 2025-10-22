51ef1b4bf73561c82413c0cc1b88cab098488298
Kouka <mohamedhassansabbagh2005@gmail.com>
2025-10-22 10:28:23 +0300

    Update EmployeeRole.java

--- src/lab4/EmployeeRole.java
+++ src/lab4/EmployeeRole.java
@@ -123,6 +123,7 @@ public class EmployeeRole {
         return false;
     }
 
+    
     public void logout() {
         productsDatabase.saveToFile();
         customerProductDatabase.saveToFile();
